7ecd1573fd6ec625fb89b72ffa428ddf658b62f2
asteroide <thomas.duval@orange.com>
2015-09-04 17:12:29 +0200

    First modifications of moonclient to comply with the current Moon API.
    Change-Id: I8c5e2f6edb17409729d5735f3d49fbdc906a2ca8

---
+++ moonclient/moonclient/tenants.py
@@ -0,0 +1,199 @@
+# Copyright 2015 Open Platform for NFV Project, Inc. and its contributors
+# This software is distributed under the terms and conditions of the 'Apache-2.0'
+# license which can be found in the file 'LICENSE' in this package distribution
+# or at 'http://www.apache.org/licenses/LICENSE-2.0'.
+
+import logging
+
+from cliff.lister import Lister
+from cliff.command import Command
+
+
+class TenantList(Lister):
+    """List all tenants."""
+
+    log = logging.getLogger(__name__)
+
+    def get_parser(self, prog_name):
+        parser = super(TenantList, self).get_parser(prog_name)
+        return parser
+
+    def take_action(self, parsed_args):
+        tenants = self.app.get_url("/v3/OS-MOON/tenants", authtoken=True)
+        self.log.debug(tenants)
+        return (
+            ("id", "name", "description", "intra_authz_extension_id", "intra_admin_extension_id"),
+            ((
+                tenant_id,
+                tenants[tenant_id]["name"],
+                tenants[tenant_id]["description"],
+                tenants[tenant_id]["intra_authz_extension_id"],
+                tenants[tenant_id]["intra_admin_extension_id"],
+             )
+             for tenant_id in tenants)
+        )
+
+
+class TenantAdd(Command):
+    """Add a tenant."""
+
+    log = logging.getLogger(__name__)
+
+    def get_parser(self, prog_name):
+        parser = super(TenantAdd, self).get_parser(prog_name)
+        parser.add_argument(
+            'tenant_name',
+            metavar='<tenant-name>',
+            help='Tenant name',
+        )
+        parser.add_argument(
+            '--authz',
+            metavar='<authz-intraextension-uuid>',
+            help='Authz IntraExtension UUID',
+        )
+        parser.add_argument(
+            '--admin',
+            metavar='<admin-intraextension-uuid>',
+            help='Admin IntraExtension UUID',
+        )
+        parser.add_argument(
+            '--desc',
+            metavar='<tenant-description-str>',
+            help='Tenant description',
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        post_data = dict()
+        post_data["tenant_name"] = parsed_args.tenant_name
+        if parsed_args.authz:
+            post_data["tenant_intra_authz_extension_id"] = parsed_args.authz
+        if parsed_args.admin:
+            post_data["tenant_intra_admin_extension_id"] = parsed_args.admin
+        if parsed_args.desc:
+            post_data["description"] = parsed_args.desc
+        tenants = self.app.get_url("/v3/OS-MOON/tenants",
+                                post_data=post_data,
+                                authtoken=True)
+        return (
+            ("id", "name", "description", "intra_authz_extension_id", "intra_admin_extension_id"),
+            ((
+                tenant_id,
+                tenants[tenant_id]["name"],
+                tenants[tenant_id]["description"],
+                tenants[tenant_id]["intra_authz_extension_id"],
+                tenants[tenant_id]["intra_admin_extension_id"],
+             )
+             for tenant_id in tenants)
+        )
+
+
+class TenantShow(Command):
+    """Show information about a tenant."""
+
+    log = logging.getLogger(__name__)
+
+    def get_parser(self, prog_name):
+        parser = super(TenantShow, self).get_parser(prog_name)
+        parser.add_argument(
+            'tenant_name',
+            metavar='<tenant-name>',
+            help='Tenant name',
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        tenants = self.app.get_url("/v3/OS-MOON/tenants/{}".format(parsed_args.tenant_name),
+                                authtoken=True)
+        return (
+            ("id", "name", "description", "intra_authz_extension_id", "intra_admin_extension_id"),
+            ((
+                tenant_id,
+                tenants[tenant_id]["name"],
+                tenants[tenant_id]["description"],
+                tenants[tenant_id]["intra_authz_extension_id"],
+                tenants[tenant_id]["intra_admin_extension_id"],
+             )
+             for tenant_id in tenants)
+        )
+
+
+class TenantSet(Command):
+    """Modify a tenant."""
+
+    log = logging.getLogger(__name__)
+
+    def get_parser(self, prog_name):
+        parser = super(TenantSet, self).get_parser(prog_name)
+        parser.add_argument(
+            'tenant_id',
+            metavar='<tenant-id>',
+            help='Tenant UUID',
+        )
+        parser.add_argument(
+            '--name',
+            metavar='<tenant-name>',
+            help='Tenant name',
+        )
+        parser.add_argument(
+            '--authz',
+            metavar='<authz-intraextension-uuid>',
+            help='Authz IntraExtension UUID',
+        )
+        parser.add_argument(
+            '--admin',
+            metavar='<admin-intraextension-uuid>',
+            help='Admin IntraExtension UUID',
+        )
+        parser.add_argument(
+            '--desc',
+            metavar='<tenant-description-str>',
+            help='Tenant description',
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        post_data = dict()
+        post_data["id"] = parsed_args.tenant_id
+        if parsed_args.name:
+            post_data["name"] = parsed_args.tenant_name
+        if parsed_args.authz:
+            post_data["intra_authz_extension_id"] = parsed_args.authz
+        if parsed_args.admin:
+            post_data["intra_admin_extension_id"] = parsed_args.admin
+        if parsed_args.desc:
+            post_data["description"] = parsed_args.desc
+        tenants = self.app.get_url("/v3/OS-MOON/tenants/{}".format(post_data["id"]),
+                                post_data=post_data,
+                                authtoken=True)
+        return (
+            ("id", "name", "description", "intra_authz_extension_id", "intra_admin_extension_id"),
+            ((
+                tenant_id,
+                tenants[tenant_id]["name"],
+                tenants[tenant_id]["description"],
+                tenants[tenant_id]["intra_authz_extension_id"],
+                tenants[tenant_id]["intra_admin_extension_id"],
+             )
+             for tenant_id in tenants)
+        )
+
+
+class TenantDelete(Command):
+    """Modify a tenant."""
+
+    log = logging.getLogger(__name__)
+
+    def get_parser(self, prog_name):
+        parser = super(TenantDelete, self).get_parser(prog_name)
+        parser.add_argument(
+            'tenant_id',
+            metavar='<tenant-id>',
+            help='Tenant UUID',
+        )
+        return parser
+
+    def take_action(self, parsed_args):
+        self.app.get_url("/v3/OS-MOON/tenants/{}".format(parsed_args.tenant_id),
+                                method="DELETE",
+                                authtoken=True)
--- moonclient/moonclient/tests.py
+++ moonclient/moonclient/tests.py
@@ -1,1 +1,56 @@
-__author__ = 'vdsq3226'
+# Copyright 2015 Open Platform for NFV Project, Inc. and its contributors
+# This software is distributed under the terms and conditions of the 'Apache-2.0'
+# license which can be found in the file 'LICENSE' in this package distribution
+# or at 'http://www.apache.org/licenses/LICENSE-2.0'.
+
+import logging
+import json
+import shlex
+import re
+from cliff.lister import Lister
+from cliff.command import Command
+
+
+class TestsLaunch(Lister):
+    """List all Intra_Extensions."""
+
+    log = logging.getLogger(__name__)
+    result_vars = dict()
+
+    def get_parser(self, prog_name):
+        parser = super(TestsLaunch, self).get_parser(prog_name)
+        parser.add_argument(
+            '--testfile',
+            metavar='<filename>',
+            help='Filename that contains tests to run',
+        )
+        return parser
+
+    def __replace_var_in_str(self, data_str):
+        for exp in re.findall("\$\w+"):
+            if exp.replace("$", "") in self.result_vars:
+                data_str.replace(exp, self.result_vars[exp.replace("$", "")])
+        return data_str
+
+    def __compare_results(self, expected, observed):
+        if expected in observed:
+            return True
+        return False
+
+    def take_action(self, parsed_args):
+        tests_dict = json.load(open(parsed_args.testfile))
+        data = list()
+        for group_name, tests_list in tests_dict.iteritems():
+            for test in tests_list:
+                data_tmp = list()
+                command = self.__replace_var_in_str(test["command"])
+                result = self.app.run_subcommand(shlex.split(command))
+                data_tmp.append(test["name"])
+                data_tmp.append(self.__compare_results(test["result"], result))
+                data_tmp.append(test["description"])
+                data.append(data_tmp)
+
+        return (
+            ("test_name", "result", "description"),
+            data
+        )
