ae5d19039729c625b578df9a5d0a475adc1f0226
asteroide <thomas.duval@orange.com>
2017-07-19 10:10:11 +0200

    Update code to put it in pypi
    Change-Id: Ia6e605c8940d7a5bbfdc14562911a0f5aa7597d7

--- moonv4/moon_orchestrator/setup.py
+++ moonv4/moon_orchestrator/setup.py
@@ -7,6 +7,9 @@ from setuptools import setup, find_packages
 import moon_orchestrator
 
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
 
     name='moon_orchestrator',
@@ -23,7 +26,7 @@ setup(
 
     long_description=open('README.rst').read(),
 
-    # install_requires= ,
+    install_requires=required,
 
     include_package_data=True,
 
