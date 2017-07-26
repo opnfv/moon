839b44d100115a01465de150fed9e901695acd21
asteroide <thomas.duval@orange.com>
2017-07-26 11:38:42 +0200

    Update build script
    Change-Id: Ia6d65fc562859a373b6c81136f5ba5a24a705d30

--- moonv4/bin/build_all.sh
+++ moonv4/bin/build_all.sh
@@ -1,36 +1,36 @@
 #!/usr/bin/env bash
 
+VERSION=v4.1
+export DOCKER_HOST=tcp://172.88.88.1:2376
+
 
 mkdir $MOON_HOME/moon_orchestrator/dist 2>/dev/null
 
+echo Building Moon_Orchestrator
+cd $MOON_HOME/moon_orchestrator
+docker build -t wukongsun/moon_orchestrator:${VERSION} .
+
 echo Building Moon_Interface
 cd $MOON_HOME/moon_interface
-python3 setup.py sdist > /tmp/moon_interface.log
-mv dist/*.tar.gz $MOON_HOME/moon_orchestrator/dist
+docker build -t wukongsun/moon_interface:${VERSION} .
 
 echo Building Moon_Security_Router
 cd $MOON_HOME/moon_secrouter
-python3 setup.py sdist > /tmp/moon_secrouter.log
-mv dist/*.tar.gz $MOON_HOME/moon_orchestrator/dist
+docker build -t wukongsun/moon_router:${VERSION} .
 
 echo Building Moon_Manager
 cd $MOON_HOME/moon_manager
-python3 setup.py sdist > /tmp/moon_manager.log
-mv dist/*.tar.gz $MOON_HOME/moon_orchestrator/dist
-
-echo Building Moon_DB
-cd $MOON_HOME/moon_db
-python3 setup.py sdist > /tmp/moon_db.log
-mv dist/*.tar.gz $MOON_HOME/moon_orchestrator/dist
-
-echo Building Moon_Utilities
-cd $MOON_HOME/moon_utilities
-python3 setup.py sdist > /tmp/moon_utilities.log
-mv dist/*.tar.gz $MOON_HOME/moon_orchestrator/dist
+docker build -t wukongsun/moon_manager:${VERSION} .
 
 echo Building Moon_Authz
 cd $MOON_HOME/moon_authz
-python3 setup.py sdist > /tmp/moon_authz.log
-mv dist/*.tar.gz $MOON_HOME/moon_orchestrator/dist
+docker build -t wukongsun/moon_authz:${VERSION} .
 
-# TODO: build container images
+
+echo Building Moon_DB
+cd $MOON_HOME/moon_db
+python3 setup.py sdist bdist_wheel > /tmp/moon_db.log
+
+echo Building Moon_Utilities
+cd $MOON_HOME/moon_utilities
+python3 setup.py sdist bdist_wheel > /tmp/moon_utilities.log
