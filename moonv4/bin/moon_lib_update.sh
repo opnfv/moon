13fe63cb3dd8b5916f48f75be107afb87f40ce96
Thomas Duval <thomas.duval@orange.com>
2017-12-08 10:36:39 +0100

    Update script to use it with moon_utilities and moon_db
    Change-Id: I89b78277a6b00f50a243cd5561280136d6b53942

--- moonv4/bin/moon_lib_update.sh
+++ moonv4/bin/moon_lib_update.sh
@@ -1,26 +1,34 @@
 #!/usr/bin/env bash
 
-VERSION=moon_utilities-$(grep __version__ moon_utilities/__init__.py | cut -d "\"" -f 2)
+# usage: moon_update.sh {build,upload,copy} {db,utilities} <GPG_ID>
+
+CMD=$1
+COMPONENT=$2
+GPG_ID=$3
+
+VERSION=moon_${COMPONENT}-$(grep __version__ moon_${COMPONENT}/moon_${COMPONENT}/__init__.py | cut -d "\"" -f 2)
+
+cd moon_${COMPONENT}
 
 python3 setup.py sdist bdist_wheel
 
-if [ "$1" = "upload" ]; then
+if [ "$CMD" = "upload" ]; then
     # Instead of "A0A96E75", use your own GPG ID
     rm dist/*.asc 2>/dev/null
-    gpg --detach-sign -u "A0A96E75" -a dist/${VERSION}-py3-none-any.whl
-    gpg --detach-sign -u "A0A96E75" -a dist/${VERSION}.tar.gz
+    gpg --detach-sign -u "${GPG_ID}" -a dist/${VERSION}-py3-none-any.whl
+    gpg --detach-sign -u "${GPG_ID}" -a dist/${VERSION}.tar.gz
     twine upload dist/${VERSION}-py3-none-any.whl dist/${VERSION}-py3-none-any.whl.asc
     twine upload dist/${VERSION}.tar.gz dist/${VERSION}.tar.gz.asc
 fi
 
-rm -f ../moon_manager/dist/moon_utilities*
-rm -f ../moon_orchestrator/dist/moon_utilities*
-rm -f ../moon_wrapper/dist/moon_utilities*
-rm -f ../moon_interface/dist/moon_utilities*
-rm -f ../moon_authz/dist/moon_utilities*
+rm -f ../moon_manager/dist/moon_${COMPONENT}*
+rm -f ../moon_orchestrator/dist/moon_${COMPONENT}*
+rm -f ../moon_wrapper/dist/moon_${COMPONENT}*
+rm -f ../moon_interface/dist/moon_${COMPONENT}*
+rm -f ../moon_authz/dist/moon_${COMPONENT}*
 
 
-if [ "$1" = "copy" ]; then
+if [ "$CMD" = "copy" ]; then
     mkdir -p ../moon_manager/dist/ 2>/dev/null
     cp -v dist/${VERSION}-py3-none-any.whl ../moon_manager/dist/
     mkdir -p ../moon_orchestrator/dist/ 2>/dev/null
