0d82ad41a698a1f10512a06d2e5a71e9bb6465bc
sgdt6900 <rhanafy.ext@orange.com>
2017-12-25 15:39:59 +0200

    adding unit tests for cache module Signed-off-by: sgdt6900 <rhanafy.ext@orange.com>
    Change-Id: I233714a7f64ff90ab306bbb6b8688525f5e8a5da

---
+++ python_moonutilities/tests/unit_python/mock_repo/urls.py
@@ -0,0 +1,147 @@
+import mock_repo.components_utilities as comp_util
+import mock_repo.data as data_mock
+
+
+def register_components(m):
+    for component in data_mock.components:
+        m.register_uri(
+            'GET', 'http://consul:8500/v1/kv/{}'.format(component),
+            json=[{'Key': component, 'Value': comp_util.get_b64_conf(component)}]
+        )
+    m.register_uri(
+        'GET', 'http://consul:8500/v1/kv/components_port_start',
+        json=[{'Key': 'components_port_start', 'Value': comp_util.get_b64_conf("components/port_start")}]
+    )
+    m.register_uri(
+        'PUT', 'http://consul:8500/v1/kv/components_port_start',
+        json=[]
+    )
+
+    m.register_uri(
+        'GET', 'http://consul:8500/v1/kv/components?recurse=true',
+        json=[
+            {"Key": key, "Value": comp_util.get_b64_conf(key)} for key in data_mock.components
+        ],
+        # json={'Key': "components", 'Value': get_b64_comp_util.CONF("components")}
+    )
+
+
+def register_keystone(m):
+    m.register_uri(
+        'POST', 'http://keystone:5000/v3/auth/tokens',
+        headers={'X-Subject-Token': "111111111"}
+    )
+    m.register_uri(
+        'DELETE', 'http://keystone:5000/v3/auth/tokens',
+        headers={'X-Subject-Token': "111111111"}
+    )
+    m.register_uri(
+        'POST', 'http://keystone:5000/v3/users?name=testuser&domain_id=default',
+        json={"users": {}}
+    )
+    m.register_uri(
+        'GET', 'http://keystone:5000/v3/users?name=testuser&domain_id=default',
+        json={"users": {}}
+    )
+    m.register_uri(
+        'POST', 'http://keystone:5000/v3/users/',
+        json={"users": [{
+            "id": "1111111111111"
+        }]}
+    )
+
+def register_model_any(m, module_name, mocked_data, key=None):
+    if key is None:
+        key = module_name
+    m.register_uri(
+        'GET', 'http://{}:{}/{}'.format(comp_util.CONF['components']['manager']['hostname'],
+                                        comp_util.CONF['components']['manager']['port'], module_name),
+
+        json={key: mocked_data}
+    )
+
+def register_policy_any(m, policy_id, module_name, mocked_data, key=None):
+    if key is None:
+        key = module_name
+    m.register_uri(
+        'GET', 'http://{}:{}/{}/{}/{}'.format(comp_util.CONF['components']['manager']['hostname'],
+                                                    comp_util.CONF['components']['manager']['port'], 'policies',
+                                                    policy_id, module_name),
+        json={key: mocked_data}
+    )
+
+def register_pdp(m):
+    register_model_any(m, 'pdp', data_mock.pdp_mock,'pdps')
+
+def register_meta_rules(m):
+    register_model_any(m, 'meta_rules',data_mock.meta_rules_mock)
+
+def register_policies(m):
+    register_model_any(m, 'policies', data_mock.policies_mock)
+
+
+def register_models(m):
+    register_model_any(m, 'models', data_mock.models_mock)
+
+def register_policy_subject(m, policy_id):
+    register_policy_any(m, policy_id, 'subjects', data_mock.subject_mock[policy_id])
+
+
+def register_policy_subject_invalid_response(m, policy_id):
+    register_policy_any(m, policy_id, 'subjects', data_mock.subject_mock[policy_id],'subjects_invalid_key')
+
+def register_policy_object(m, policy_id):
+    register_policy_any(m, policy_id, 'objects', data_mock.object_mock[policy_id])
+
+def register_policy_action(m, policy_id):
+    register_policy_any(m, policy_id, 'actions', data_mock.action_mock[policy_id])
+
+def register_policy_subject_assignment_list(m, policy_id):
+    register_policy_any(m, policy_id, 'subject_assignments', data_mock.subject_assignment_mock)
+
+def register_policy_object_assignment_list(m, policy_id):
+    register_policy_any(m, policy_id, 'object_assignments', data_mock.object_assignment_mock)
+
+
+def register_policy_action_assignment_list(m, policy_id):
+    register_policy_any(m, policy_id, 'action_assignments', data_mock.action_assignment_mock)
+
+def register_policy_subject_assignment(m, policy_id, perimeter_id):
+    m.register_uri(
+        'GET', 'http://{}:{}/{}/{}/subject_assignments/{}'.format(comp_util.CONF['components']['manager']['hostname'],
+                                                                  comp_util.CONF['components']['manager']['port'],
+                                                                  'policies',
+                                                                  policy_id,
+                                                                  perimeter_id),
+        json={'subject_assignments': data_mock.subject_assignment_mock}
+    )
+
+def register_policy_object_assignment(m, policy_id, perimeter_id):
+    m.register_uri(
+        'GET', 'http://{}:{}/{}/{}/object_assignments/{}'.format(comp_util.CONF['components']['manager']['hostname'],
+                                                                 comp_util.CONF['components']['manager']['port'],
+                                                                 'policies',
+                                                                 policy_id,
+                                                                 perimeter_id),
+        json={'object_assignments': data_mock.object_assignment_mock}
+    )
+
+def register_policy_action_assignment(m, policy_id, perimeter_id):
+    m.register_uri(
+        'GET', 'http://{}:{}/{}/{}/action_assignments/{}'.format(comp_util.CONF['components']['manager']['hostname'],
+                                                                 comp_util.CONF['components']['manager']['port'],
+                                                                 'policies',
+                                                                 policy_id,
+                                                                 perimeter_id),
+        json={'action_assignments': data_mock.action_assignment_mock}
+    )
+
+def register_rules(m, policy_id):
+    register_policy_any(m, policy_id, 'rules', data_mock.rules_mock)
+
+# def register_pods(m):
+#     m.register_uri(
+#         'GET', 'http://{}:{}/pods'.format(comp_util.CONF['components']['orchestrator']['hostname'],
+#                                               comp_util.CONF['components']['orchestrator']['port']),
+#         json={'pods': data_mock.pods_mock}
+#     )
--- python_moonutilities/tests/unit_python/test_configuration.py
+++ python_moonutilities/tests/unit_python/test_configuration.py
@@ -1,5 +1,54 @@
+import mock_repo.components_utilities as comp_util
+import pytest
+import requests_mock
+
+
+
+def test_get_configuration_success():
+    from moon_utilities import configuration
+    assert configuration.get_configuration("components/port_start")["components/port_start"] == comp_util.CONF["components"]["port_start"]
+
+@requests_mock.Mocker(kw='mock')
+def test_get_configuration_not_found(**kwargs):
+    from moon_utilities import configuration
+
+    kwargs['mock'].get('http://consul:8500/v1/kv/components/port_start_wrong', json=[
+    ], status_code=500)
+    with pytest.raises(Exception) as exception_info:
+        configuration.get_configuration("components/port_start_wrong")
+    assert str(exception_info.value) == '500: Consul error'
+
+# [TODO] this test used to test the invalid response
+# it should be un commented and run after refactoring the related part
+@requests_mock.Mocker(kw='mock')
+def test_get_configuration_invalid_response(**kwargs):
+    from moon_utilities import configuration
+
+    kwargs['mock'].get('http://consul:8500/v1/kv/components_port_start', json=[
+        {"components_port_start":'components_port_start', 'Value': comp_util.get_b64_conf("components/port_start")}
+    ])
+    # with pytest.raises(Exception) as exception_info:
+    # configuration.get_configuration("components_port_start")
+    # assert str(exception_info.value) == '500: Consul error'
+
+@requests_mock.Mocker(kw='mock')
+def test_put_increment_port_failure(**kwargs):
+    from moon_utilities import configuration
+    kwargs['mock'].put('http://consul:8500/v1/kv/components_port_start', json=[], status_code=400)
+    kwargs['mock'].get('http://consul:8500/v1/kv/components_port_start', json=[
+        {'Key': 'components_port_start', 'Value': comp_util.get_b64_conf("components/port_start")}
+    ], status_code=200)
+    with pytest.raises(Exception) as exception_info:
+        configuration.increment_port()
+    assert str(exception_info.value) == '400: Consul error'
+
+def test_increment_port_success():
+    from moon_utilities import configuration
+    cur_port = comp_util.CONF["components"]["port_start"]
+    incremented_port = configuration.increment_port()
+    assert incremented_port  == cur_port + 1
+
 
 def test_get_components():
-    from python_moonutilities import configuration
+    from moon_utilities import configuration
     assert isinstance(configuration.get_components(), dict)
-
