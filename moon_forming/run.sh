14a2b25f60a537609ba4255534a9b1fcceffca99
RHE <rebirthmonkey@gmail.com>
2017-12-27 09:58:36 +0100

    moon_forming for policy templates
    Change-Id: I8f19a9c4d791d509a6c0349424252ea24c56ab6b
    Signed-off-by: RHE <rebirthmonkey@gmail.com>

--- moon_forming/run.sh
+++ moon_forming/run.sh
@@ -5,8 +5,9 @@ populate_args=$*
 echo "Waiting for Consul (http://consul:8500)"
 while ! python -c "import requests; req = requests.get('http://consul:8500')" 2>/dev/null ; do
     sleep 5 ;
-    echo "."
+    echo -n "."
 done
+echo "."
 echo "Consul (http://consul:8500) is up."
 
 python3 /root/conf2consul.py /etc/moon/moon.conf
@@ -14,8 +15,9 @@ python3 /root/conf2consul.py /etc/moon/moon.conf
 echo "Waiting for DB (tcp://db:3306)"
 while ! python -c "import socket, sys; s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.connect(('db', 3306)); sys.exit(0)" 2>/dev/null ; do
     sleep 5 ;
-    echo "."
+    echo -n "."
 done
+echo "."
 echo "Database (http://db:3306) is up."
 
 moon_db_manager upgrade
@@ -23,15 +25,17 @@ moon_db_manager upgrade
 echo "Waiting for Keystone (http://keystone:5000)"
 while ! python -c "import requests; req = requests.get('http://keystone:5000')" 2>/dev/null ; do
     sleep 5 ;
-    echo "."
+    echo -n "."
 done
+echo "."
 echo "Keystone (http://keystone:5000) is up."
 
 echo "Waiting for Manager (http://manager:8082)"
 while ! python -c "import requests; req = requests.get('http://manager:8082')" 2>/dev/null ; do
     sleep 5 ;
-    echo "."
+    echo -n "."
 done
+echo "."
 echo "Manager (http://manager:8082) is up."
 
 for i in /data/*.py ; do
