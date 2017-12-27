fa4a7506d7c331a68f9df4c1418ca56ff5404f65
sgdt6900 <rhanafy.ext@orange.com>
2017-12-27 13:37:46 +0200

    refactoring cache module solving issue adding request wrapper adding more exception types
    Change-Id: I4a04b478607b3928d3be0e175c202662d52288c1
    Signed-off-by: sgdt6900 <rhanafy.ext@orange.com>

---
+++ python_moonutilities/python_moonutilities/request_wrapper.py
@@ -0,0 +1,12 @@
+import sys
+import requests
+from python_moonutilities import exceptions
+
+def get(url):
+    try:
+        response = requests.get(url)
+    except requests.exceptions.RequestException as e:
+        raise exceptions.ConsulError("request failure ",e)
+    except:
+        raise exceptions.ConsulError("Unexpected error ", sys.exc_info()[0])
+    return response
