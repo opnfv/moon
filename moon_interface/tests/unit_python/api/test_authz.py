8f6023b826d8b10e19018c4d0585f87d27314be4
sgdt6900 <rhanafy.ext@orange.com>
2018-01-09 11:33:47 +0200

    adding comments for some issues and points
    refactor retry method
    
    apply the comments notes
    
    adding test cases
    
    adding more comments
    
    adding test cases
    
    adding more comments , refactoring, tests
    Change-Id: I0949fcaed2b88f3cf75e66b6a90e6e9d7ca156b1
    Signed-off-by: sgdt6900 <rhanafy.ext@orange.com>

--- moon_interface/tests/unit_python/api/test_authz.py
+++ moon_interface/tests/unit_python/api/test_authz.py
@@ -1,4 +1,5 @@
 import json
+import conftest
 
 
 def get_json(data):
@@ -6,6 +7,7 @@ def get_json(data):
 
 
 def test_authz_true(context):
+
     import moon_interface.server
     server = moon_interface.server.create_server()
     client = server.app.test_client()
@@ -19,5 +21,61 @@ def test_authz_true(context):
     data = get_json(req.data)
     assert data
     assert "result" in data
-    assert data['result'] == True
+    assert data['result'] is True
 
+def test_authz_False(context):
+    import moon_interface.server
+    server = moon_interface.server.create_server()
+    client = server.app.test_client()
+
+    req = client.get("/authz/{p_id}/{s_id}/{o_id}/{a_id}".format(
+        p_id=None,
+        s_id=context["subject_name"],
+        o_id=context["object_name"],
+        a_id=context["action_name"],
+    ))
+    assert req.status_code == 403
+    data = get_json(req.data)
+    assert data
+    assert "result" in data
+    assert data['result'] is False
+
+
+def test_authz_effect_unset(context, set_consul_and_db):
+    import moon_interface.server
+    server = moon_interface.server.create_server()
+    client = server.app.test_client()
+
+    set_consul_and_db.register_uri(
+        'POST', 'http://127.0.0.1:8081/authz',
+        content = conftest.get_pickled_context_invalid()
+    )
+
+    req = client.get("/authz/{p_id}/{s_id}/{o_id}/{a_id}".format(
+        p_id=context["pdp_id"],
+        s_id=context["subject_name"],
+        o_id=context["object_name"],
+        a_id=context["action_name"],
+    ))
+    assert req.status_code == 401
+    data = get_json(req.data)
+    assert data
+    assert "result" in data
+    assert data['result'] is False
+
+def test_authz_invalid_ip(context, set_consul_and_db):
+    import moon_interface.server
+    server = moon_interface.server.create_server()
+    client = server.app.test_client()
+
+    set_consul_and_db.register_uri(
+        'POST', 'http://127.0.0.1:8081/authz', status_code=500
+    )
+
+    req = client.get("/authz/{p_id}/{s_id}/{o_id}/{a_id}".format(
+        p_id=context["pdp_id"],
+        s_id=context["subject_name"],
+        o_id=context["object_name"],
+        a_id=context["action_name"],
+    ))
+    assert req.status_code == 403
