acb811aafc97d60cdcc71e0b9b2466fb00bfb097
Thomas Duval <thomas.duval@orange.com>
2018-03-14 10:55:49 +0100

    Add the ability to force the installation of specific moon libraries in dist directory.
    Change-Id: I430655b1ff80dcb8b13a66ae58802fc97e94cdd0

--- moon_pythonunittest/run_tests.sh
+++ moon_pythonunittest/run_tests.sh
@@ -4,6 +4,12 @@ cd /data
 pip3 install -r tests/unit_python/requirements.txt --upgrade
 pip3 install .
 
+if [ -d /data/dist ];
+then
+    pip install /data/dist/*.tar.gz --upgrade
+    pip install /data/dist/*.whl --upgrade
+fi
+
 if [ -f /data/tests/unit_python/run_tests.sh ];
 then
     bash /data/tests/unit_python/run_tests.sh;
