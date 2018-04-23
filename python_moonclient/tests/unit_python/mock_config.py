35885501dc6d6a5423501b8dfa481abe28b475f8
Thomas Duval <thomas.duval@orange.com>
2018-04-23 12:31:22 +0200

    Fix a bug occuring only in Jenkins
    Change-Id: I2ba0c6e435e62450f9da57846ca05aeddf73f4d0

--- python_moonclient/tests/unit_python/mock_config.py
+++ python_moonclient/tests/unit_python/mock_config.py
@@ -38,3 +38,27 @@ def register_consul(m):
         'GET', 'http://manager:30001',
         json={}
     )
+    m.register_uri(
+        'GET', 'http://keystone:5000/v3',
+        json={}
+    )
+    m.register_uri(
+        'POST', 'http://keystone:5000/v3/auth/tokens',
+        headers={'X-Subject-Token': "111111111"}
+    )
+    m.register_uri(
+        'DELETE', 'http://keystone:5000/v3/auth/tokens',
+        headers={'X-Subject-Token': "111111111"}
+    )
+    m.register_uri(
+        'POST', 'http://keystone:5000/v3/users?name=testuser&domain_id=default',
+        json={"users": {}}
+    )
+    m.register_uri(
+        'GET', 'http://keystone:5000/v3/users?name=testuser&domain_id=default',
+        json={"users": {}}
+    )
+    m.register_uri(
+        'POST', 'http://keystone:5000/v3/users/',
+        json={"users": [{"id": "1111111111111"}]}
+    )
